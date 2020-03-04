6214a7da20af5c7d996552180609bcb532bcf532
Piotrek <piotrek.adamczykk@gmail.com>
2020-03-05 00:53:49 +0100

    Clean up the trash

--- src/main/java/contactbook/application/services/SecurityService.java
+++
@@ -1,6 +0,0 @@
-//package contactbook.application.services;
-//
-//public interface SecurityService {
-//    String findLoggedInUsername();
-//    void autoLogin(String username, String password);
-//}
